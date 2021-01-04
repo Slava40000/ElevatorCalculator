e3d6b31cb5bf2be2019aebaf035b5121e1c1071e
slava <luidzit@gmail.com>
2021-01-04 18:09:22 +0300

    translate + js in file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 // nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
+    
+    function CalculateTime(){
+        let floor = document.getElementById('floor-number').value;
+        let speed = document.getElementById('elevators-speed').value;
+        let rides = document.getElementById('rides-per-day').value;
+        alert("Каждый месяц " + floor*speed*rides*30 + " секунд");
+}
