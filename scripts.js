711aff66c90b4bc4dac7fe9e2844877ed5b770d6
Mark <fominulya@yahoo.com>
2021-01-05 17:42:59 +0300

    js formula simplification and html fixes

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-// nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
-    
-    function CalculateTime(){
-        let floor = document.getElementById('floor-number').value;
-        let speed = document.getElementById('elevators-speed').value;
-        let rides = document.getElementById('rides-per-day').value;
-        let days = document.getElementById('trips-in-x-days').value;
-        alert("Столько времени я еду в лифте " + floor*speed*rides*days + " секунд");
+function CalculateTime(){
+    let floor = document.getElementById('floor-number').value;
+    let speed = document.getElementById('elevators-speed').value;
+    let rides = document.getElementById('rides-per-day').value;
+    let days = document.getElementById('number-of-days').value;
+    let timeInElevator = floor*speed*rides*days
+    alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator/60 + " минут");
 }
+
+// nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
