52448accd4ded856e3c0033b89659733a4805dce
Mark <fominulya@yahoo.com>
2021-01-05 22:00:40 +0300

    naming corrections

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function CalculateTime() {
     floor = document.getElementById('floor-number').value;
     speed = document.getElementById('elevators-speed').value;
     rides = document.getElementById('rides-per-day').value;
-    days = document.getElementById('number-of-days').value;
+    days = document.getElementById('time-interval').value;
     // randomNumber = Math.ceil(Math.random() * 20);
     randomNumber = Math.floor(Math.random() * 10) + 1; // не должно генерировать нули
     timeInElevator = floor * speed * rides * days * randomNumber;
