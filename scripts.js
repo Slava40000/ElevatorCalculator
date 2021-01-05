5edea98391bb40d13220b4041266229e42b6f2ad
Mark <fominulya@yahoo.com>
2021-01-05 21:48:18 +0300

    variables declaration

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-function CalculateTime(){
-    let floor = document.getElementById('floor-number').value;
-    let speed = document.getElementById('elevators-speed').value;
-    let rides = document.getElementById('rides-per-day').value;
-    let days = document.getElementById('number-of-days').value;
-    let randomNumber = Math.ceil (Math.random () * 20); 
-    let timeInElevator = floor*speed*rides*days*randomNumber
-    alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator/60 + " минут");
+let floor, speed, rides, days, randomNumber, timeInElevator
+
+function CalculateTime() {
+    floor = document.getElementById('floor-number').value;
+    speed = document.getElementById('elevators-speed').value;
+    rides = document.getElementById('rides-per-day').value;
+    days = document.getElementById('number-of-days').value;
+    // randomNumber = Math.ceil(Math.random() * 20);
+    randomNumber = Math.floor(Math.random() * 10) + 1; // не должно генерировать нули
+    timeInElevator = floor * speed * rides * days * randomNumber;
+    alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут");
 }
 
 // nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
-
