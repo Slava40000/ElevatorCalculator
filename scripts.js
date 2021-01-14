4946cb4a2e4b226a0423d9ca64c888261a571ea5
slava <luidzit@gmail.com>
2021-01-14 22:33:38 +0300

    add random checkbox and alerts

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
-let floor, speed, rides, days, randomNumber, timeInElevator
+let floor, speed, rides, days, randomCheckbox, randomNumber, timeInElevator, maxFloor
 
 function CalculateTime() {
     floor = document.getElementById('floor-number').value;
-      if ( floor > 20) alert( 'Вы врете!' );
+    maxFloor = document.getElementById('max-floor-number').value;
     speed = document.getElementById('elevators-speed').value;
     rides = document.getElementById('rides-per-day').value;
     days = document.getElementById('time-interval').value;
-    // randomNumber = Math.ceil(Math.random() * 20);
-    randomNumber = Math.floor(Math.random() * 10) + 1; // не должно генерировать нули
-    timeInElevator = floor * speed * rides * days * randomNumber;
-    alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут");
+    randomCheckbox = document.getElementById('random-checkbox').checked;
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    timeInElevator = floor * speed * rides * days * randomCheckbox ? randomNumber : 1;
+    floor > maxFloor ? alert("Лжец и подлец" ) : alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут");
 }
 
 // nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
