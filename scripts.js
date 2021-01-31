b6c37a860698a68c0082068fdadf6faa9539e472
slava <luidzit@gmail.com>
2021-01-31 22:54:25 +0300

    onpage text + calculation error

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 let floor, speed, rides, days, randomCheckbox, randomNumber, timeInElevator, maxFloor
 
 function CalculateTime() {
-    floor = document.getElementById('floor-number').value;
-    maxFloor = document.getElementById('max-floor-number').value;
+    floor = +document.getElementById('floor-number').value;
+    maxFloor = +document.getElementById('max-floor-number').value;
     speed = document.getElementById('elevators-speed').value;
     rides = document.getElementById('rides-per-day').value;
     days = document.getElementById('time-interval').value;
     randomCheckbox = document.getElementById('random-checkbox').checked;
     randomNumber = Math.floor(Math.random() * 10) + 1;
     timeInElevator = floor * speed * rides * days * randomCheckbox ? randomNumber : 1;
-    floor > maxFloor ? alert("Лжец и подлец" ) : alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут");
+    // floor > maxFloor ? alert("Лжец и подлец" ) : alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут");
+    // result = timeInElevator;
+    result = floor > maxFloor ? "Лжец и подлец" : "Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут";
+    document.getElementById("itog").innerHTML = result;
 }
 
 // nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
+
