213f67e4c27dea69c2ff83043eabbe23f86cb1b5
Mark <fominulya@yahoo.com>
2021-03-02 23:17:13 +0300

    Calculator bug fixed

--- scripts.js
+++ scripts.js
@@ -3,16 +3,15 @@ let floor, speed, rides, days, randomCheckbox, randomNumber, timeInElevator, max
 function CalculateTime() {
     floor = +document.getElementById('floor-number').value;
     maxFloor = +document.getElementById('max-floor-number').value;
-    speed = document.getElementById('elevators-speed').value;
+    speed = document.getElementById('elevator-speed').value;
     rides = document.getElementById('rides-per-day').value;
     days = document.getElementById('time-interval').value;
     randomCheckbox = document.getElementById('random-checkbox').checked;
-    randomNumber = Math.floor(Math.random() * 10) + 1;
-    timeInElevator = floor * speed * rides * days * randomCheckbox ? randomNumber : 1;
-    // floor > maxFloor ? alert("Лжец и подлец" ) : alert("Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут");
+    randomNumber = randomCheckbox ? Math.floor(Math.random() * 10) + 1 : 1;
+    timeInElevator = floor * speed * rides * days * randomNumber;
     // result = timeInElevator;
     result = floor > maxFloor ? "Лжец и подлец" : "Время езды в лифте: " + timeInElevator + " секунд или " + timeInElevator / 60 + " минут";
-    document.getElementById("itog").innerHTML = result;
+    document.getElementById("time-result").innerHTML = result;
 }
 
 // nomer-etaja.addEventListener('input', e => span.innerText = e.target.value)
